f6fe690a30207ea49446a340eee1c50c243ee287
panthil malaviya <panthil@dstrctgroup.com>
2024-08-27 18:59:09 +0530

    update batches

--- services/batches/batches.services.js
+++ services/batches/batches.services.js
@@ -118,7 +118,7 @@ module.exports = {
         },
       ]);
       let allBatchesStudentsCount = await studentsModel.countDocuments({
-        collegeUserId: collegeId,
+        collegeUserId: new ObjectId(collegeId),
       });
       batches.unshift({
         label: "All",
