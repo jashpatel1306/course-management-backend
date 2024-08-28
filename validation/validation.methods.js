7c06b06200899c41366f377cefa10f23dfe770a1
dev-nikunj <devnikunj21@gmail.com>
2024-08-28 17:22:39 +0530

    validation file change

--- validation/validation.methods.js
+++ validation/validation.methods.js
@@ -5,14 +5,14 @@ const Validate = (schema) => {
       const requestData = req?.body;
       console.log("requestData", requestData);
       if (isset(requestData)) {
-        const data = {data:requestData};
-        // const data =
-        //   process.env.NODE_ENV === "development"
-        //     ? { data: JSON.parse(requestData.data) }
-        //     : { data: requestData };
+        // const data = {data:requestData};
+        const data =
+          process.env.NODE_ENV === "development"
+            ? { data: JSON.parse(requestData.data) }
+            : { data: requestData };
         console.log("data", data.data);
 
-        const result = await schema.validate(data.data); 
+        const result = await schema.validate(data.data);
         if (result.error) {
           return res.status(200).json({
             status: false,
