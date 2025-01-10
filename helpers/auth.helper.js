ba1b7bb735869800ab50fc20a15fe262d12b515d
panthil288 <panthilmalaviya288@gmail.com>
2025-01-10 19:24:46 +0530

    added error handeling

--- helpers/auth.helper.js
+++ helpers/auth.helper.js
@@ -16,80 +16,89 @@ module.exports = {
     token = token.toLowerCase().includes("bearer")
       ? token.split(" ")[1]
       : token;
-    jwt.verify(token, JWTSecretKey, async (err, result) => {
-      if (err)
-        return res.json({
-          status: false,
-          message: `Invalid token or expired!`,
-          isAuth: false,
-        });
-
-      if (result && isset(result.user_id)) {
-        const getUserData = await userServices.findUserById(result.user_id);
-        if (!getUserData)
-          return res.json({
+    try {
+      jwt.verify(token, JWTSecretKey, async (err, result) => {
+        if (err)
+          return res.status(401).json({
             status: false,
             message: `Invalid token or expired!`,
             isAuth: false,
           });
-        req.body.user_id = result.user_id;
-        req.body.college_id = result.college_id;
 
-        res.locals.userRole = result.role;
-        return next();
-      }
-      return res.json({
-        status: false,
-        message: `Invalid token or expired!`,
-        isAuth: false,
+        if (result && isset(result.user_id)) {
+          const getUserData = await userServices.findUserById(result.user_id);
+          if (!getUserData)
+            return res.status(401).json({
+              status: false,
+              message: `Invalid token or expired!`,
+              isAuth: false,
+            });
+          req.body.user_id = result.user_id;
+          req.body.college_id = result.college_id;
+
+          res.locals.userRole = result.role;
+          return next();
+        }
+        return res.status(401).json({
+          status: false,
+          message: `Invalid token or expired!`,
+          isAuth: false,
+        });
       });
-    });
+    } catch (error) {
+      console.log("Error in authentication", error);
+      next(error);
+    }
   },
   isAdminCommonAuthenticate: (req, res, next) => {
     let token = req.headers.authorization;
     token = token.toLowerCase().includes("bearer")
       ? token.split(" ")[1]
       : token;
-    jwt.verify(token, JWTSecretKey, async (err, result) => {
-      if (err) {
-        console.log("jwt error", err);
-        return res.json({
-          status: false,
-          message: `Invalid token or expired!`,
-          isAuth: false,
-        });
-      }
-
-      if (result && isset(result.user_id)) {
-        const getUserData = await userServices.findUserById(result.user_id);
-        if (!getUserData)
+    try {
+      jwt.verify(token, JWTSecretKey, async (err, result) => {
+        if (err) {
+          console.log("jwt error", err);
           return res.json({
             status: false,
             message: `Invalid token or expired!`,
             isAuth: false,
           });
+        }
 
-        if ([INSTRUCTOR, STUDENT].includes(getUserData?.role))
-          return res.json({
-            status: false,
-            message: `Access to the target resource has been denied`,
-            isAuth: false,
-          });
-        req.body = {
-          ...req.body,
-          user_id: result.user_id,
-          college_id: result.college_id,
-        };
-        res.locals.userRole = result.role;
+        if (result && isset(result.user_id)) {
+          const getUserData = await userServices.findUserById(result.user_id);
+          if (!getUserData)
+            return res.json({
+              status: false,
+              message: `Invalid token or expired!`,
+              isAuth: false,
+            });
 
-        return next();
-      }
-      return res.json({
-        status: false,
-        message: `Invalid token or expired!`,
-        isAuth: false,
+          if ([INSTRUCTOR, STUDENT].includes(getUserData?.role))
+            return res.json({
+              status: false,
+              message: `Access to the target resource has been denied`,
+              isAuth: false,
+            });
+          req.body = {
+            ...req.body,
+            user_id: result.user_id,
+            college_id: result.college_id,
+          };
+          res.locals.userRole = result.role;
+
+          return next();
+        }
+        return res.json({
+          status: false,
+          message: `Invalid token or expired!`,
+          isAuth: false,
+        });
       });
-    });
+    } catch (error) {
+      next(error);
+    }
   },
   // =================================== Check super admin authentication  ===================================
   isSuperAdminAuthenticate: (req, res, next) => {
@@ -97,41 +106,46 @@ module.exports = {
     token = token.toLowerCase().includes("bearer")
       ? token.split(" ")[1]
       : token;
-    jwt.verify(token, JWTSecretKey, async (err, result) => {
-      if (err)
+    try {
+      jwt.verify(token, JWTSecretKey, async (err, result) => {
+        if (err)
+          return res.json({
+            status: false,
+            message: `Something is wrong in Authentication.Please try again.`,
+            isAuth: false,
+          });
+
+        if (result && isset(result.user_id)) {
+          const getUserData = await userServices.findUserById(result.user_id);
+          if (!getUserData)
+            return res.json({
+              status: false,
+              message: `Invalid token or expired!`,
+              isAuth: false,
+            });
+          if (getUserData.role === SUPERADMIN) {
+            req.body.user_id = result.user_id;
+            req.body.college_id = result.college_id;
+            res.locals.userRole = SUPERADMIN;
+            return next();
+          } else {
+            return res.json({
+              status: false,
+              message: `Access to the target resource has been denied`,
+              isAuth: false,
+            });
+          }
+        }
         return res.json({
           status: false,
-          message: `Something is wrong in Authentication.Please try again.`,
+          message: `Invalid token or expired!`,
           isAuth: false,
         });
-
-      if (result && isset(result.user_id)) {
-        const getUserData = await userServices.findUserById(result.user_id);
-        if (!getUserData)
-          return res.json({
-            status: false,
-            message: `Invalid token or expired!`,
-            isAuth: false,
-          });
-        if (getUserData.role === SUPERADMIN) {
-          req.body.user_id = result.user_id;
-          req.body.college_id = result.college_id;
-          res.locals.userRole = SUPERADMIN;
-          return next();
-        } else {
-          return res.json({
-            status: false,
-            message: `Access to the target resource has been denied`,
-            isAuth: false,
-          });
-        }
-      }
-      return res.json({
-        status: false,
-        message: `Invalid token or expired!`,
-        isAuth: false,
       });
-    });
+    } catch (error) {
+      console.log("Error in super admin authentication", error);
+      next(error);
+    }
   },
   isStudentAuthenticate: (req, res, next) => {
     let token = req.headers.authorization;
